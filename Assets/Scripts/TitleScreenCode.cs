c2bad79e4a4081174e7912813202bbb65550a127
S10267556 <s10267556@connect.np.edu.sg>
2025-08-11 16:23:52 +0800

    Added Ui
    Added functional ui to the start screen, adjusted lighting for the house interior

---
+++ Assets/Scripts/TitleScreenCode.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenCode : MonoBehaviour
+{
+    public void LoadGameScene(string sceneName)
+    {
+        // Load the specified scene when the start button is pressed
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        // Quit the application when the quit button is pressed
+        Debug.Log("Quit Game.");
+        Application.Quit();
+    }  
+}
